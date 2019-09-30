a61c7dd0d7bede85e7f1c39b5deaf8060a14e388
Christian Knauer <christiancalled@gmail.com>
2019-09-30 11:09:10 -0700

    Implement magnific popup lightbox on works page.

--- js/script.js
+++ js/script.js
@@ -16,3 +16,48 @@ let setHeaderWidth = function () {
 
   header.style.width = containerWidth + 'px';
 };
+
+
+//Required for MagnificPopup
+
+$(document).ready(function() {
+  $('#mfp-portfolio').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: '',
+    gallery: {
+      enabled: true,
+    },
+    image: {
+      titleSrc: 'title'
+    },
+    fixedContentPos: false,  // Makes page scrollable when img open.
+    mainClass: 'mfp-zoom-in',
+    removalDelay: 200, //delay removal by X to allow out-animation
+    showCloseBtn: false, // Hides default close button.
+    callbacks: {
+        beforeOpen: function() {
+            $('#portfolio a').each(function(){
+                $(this).attr('title', $(this).find('img').attr('alt'));
+            });
+        },
+        open: function() {
+            //overwrite default prev + next function. Add timeout for css3 crossfade animation
+            $.magnificPopup.instance.next = function() {
+                var self = this;
+                self.wrap.removeClass('mfp-image-loaded');
+                setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+            }
+            $.magnificPopup.instance.prev = function() {
+                var self = this;
+                self.wrap.removeClass('mfp-image-loaded');
+                setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+            }
+        },
+        imageLoadComplete: function() {
+            var self = this;
+            setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+        }
+    }
+  });
+});
