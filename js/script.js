07ed3f607fdf068016cdd042589212198ee1b1de
Christian Knauer <christiancalled@gmail.com>
2019-11-25 16:43:00 -0800

    Clean up JS a little.

--- js/script.js
+++ js/script.js
@@ -19,7 +19,8 @@
 
     header.style.width = containerWidth + 'px';
   };
-}
+} // End header width.
+
 
 // HEADER HEIGHT
 // Reduce height on scroll btw width 691px-1000px.
@@ -77,8 +78,7 @@
       header.style.transform = 'none';
     }
   });
-
-} // END HEADER HEIGHT
+} // End header height.
 
 
 // MAGNIFIC POPUP
@@ -129,269 +129,265 @@ $(document).ready(function() {
       }
     }
   });
-});
+}); // End Magnific Popup.
 
 
 // START MAIN NAVIGATION
 
-let showDropdown = (e) => {
-
-  if (e.target.classList.contains('js-nav-open')) {
-    clearTimeout(delayHide);
-
-    let show = () => {
-
-      let subMenu = document.getElementById('js-sub-menu');
-
-      // Hide all sub-menu content when hovering over menu-items.
-      let divArr = subMenu.querySelectorAll('div'); // Get all sub-menu divs.
-      for (let i = 0; i < divArr.length; i++) { // Add js-hide to all divs.
-        divArr[i].classList.add('js-hide');
-      }
-
-      // Show drop down container.
-      subMenu.classList.remove('js-hide');
-
-      // Show content-e.target
-      let triggerClass = e.target.classList[0]; // Get class that links to sub-menu content.
-      document.getElementsByClassName(triggerClass)[1].classList.remove('js-hide'); // Get 2nd elem w/that class, which is the sub-menu content, and display it.
-
-      // Shift drop down into place.
-
-      // Get a-tag position.
-      let rectTarget = e.target.getBoundingClientRect();
-      let leftEdgeOfTarget = rectTarget.left;
-      // Find drop down postion same as left edge of a-tag.
-      let headerOffsetLeft = document.getElementsByClassName('js-header')[0].offsetLeft;
-      let subMenuLeftPosition = leftEdgeOfTarget - headerOffsetLeft;
-      // Find length of drop down to center it under respective a-tag.
-      let subMenuWidth = subMenu.offsetWidth; // This has to run after we display both submenu and its child divs.
-      let aTagWidth = e.target.offsetWidth;
-      let adjustSubMenuPosition = (subMenuWidth - aTagWidth) / 2
-      // Set drop down position
-      subMenu.style.left = (subMenuLeftPosition - adjustSubMenuPosition) + 'px';
-    };
-
-    let delayShow = setTimeout(show, 200);
-  }
-};
-
-document.getElementsByTagName('nav')[0].addEventListener('mouseover', function (e) {
-  showDropdown(e);
-}, false);
-
-
-// If mouseenter on drop down cancel timeout for closing it.
-document.getElementById('js-sub-menu').addEventListener('mouseover', function () {
-  clearTimeout(delayHide);
-}, false);
-
-let delayHide;
-
-let hideDropdown = (e) => {
-  if (!e.relatedTarget.classList.contains('js-nav-open')) {
-    let hide = () => {
-      let subMenu = document.getElementById('js-sub-menu');
-      // Hide drop down container.
-      subMenu.classList.add('js-hide');
-    };
-
-    delayHide = setTimeout(hide, 600);
-  }
-};
-
-
-// Loop over all class-names 'nav-open' and add eventlistener to them.
-let navOpenArr = document.getElementsByClassName('js-nav-open');
-for (let i = 0; i < navOpenArr.length; i++) {
-  navOpenArr[i].addEventListener('mouseleave', function (e) {
-    hideDropdown(e);
-  }, false);
-};
-
-
-// Dynamic active-menu indicator for main-nav.
-(function () {
-  let currentPath = location.pathname,
-      aArr = document.querySelectorAll('nav a');
-
-  for (let i = 0; i < aArr.length; i++) {
-    // Get href of a tag.
-    let href = aArr[i].getAttribute('href');
-    // Get index position of href inside current path. If not -1 execute code.
-    if (currentPath.indexOf(href) !== -1) {
-      // Add active class to a elem.
-      aArr[i].classList.add('js-nav-a-active');
-    }
-  }
-}());
-
-// Dynamic active-menu indicator for sub-nav. And also for main nav.
-(function () {
-  let currentPath = location.pathname,
-      subMenu = document.getElementsByClassName('nav-sub-menu')[0],
-      aArr = subMenu.getElementsByTagName('a');
-
-  for (let i = 0; i < aArr.length; i++) {
-    // Get href of a tag.
-    let href = aArr[i].getAttribute('href');
-    // Get index position of href inside current path. If not -1 execute code.
-    if (currentPath.indexOf(href) !== -1) {
-      // Add active class to a elem.
-      aArr[i].classList.add('js-nav-sub-menu-active');
-
-      // Apply active-menu indicator to parent main-nav items.
-      // Get a-tags parents first class name.
-      let firstClassNameOfParent = aArr[i].parentNode.classList[0];
-      // Apply active nav css to main nav item.
-      document.getElementsByClassName(firstClassNameOfParent)[0].classList.add('js-nav-a-active');
-    }
-  }
-}());
-
-
-// If hover/leave sub nav mark/un-markt respective main nav item.
-(function() {
-  let subMenu = document.getElementById('js-sub-menu');
-  let divArr = subMenu.getElementsByTagName('div');
-
-  // If hovering over sub menu, mark respective nav item.
-  subMenu.addEventListener('mousemove', function () {
-    // Add event listener to all divs in sub menu.
-    for (let i = 0; i < divArr.length; i++) {
-      // Mark respective main nav item as active.
-      if (!divArr[i].classList.contains('js-hide')) {
-        let firstClassName = divArr[i].classList[0];
-        let mainNavItem = document.getElementsByClassName(firstClassName)[0];
-        mainNavItem.classList.add('js-nav-a-active');
-      }
-    }
-  }, false);
-
-  // If sub menu doesn't show, un-mark respective nav item.
-  subMenu.addEventListener('mouseleave', function (e) {
-    // Get number of active main-nav-items.
-    let activeArr = document.getElementsByClassName('js-nav-a-active');
-
-    for (let i = 0; i < divArr.length; i++) {
-      // Target respective sub nav div.
-      // If respective sub-nav-div contains class js-hide and
-      // if the number of active main-nav-items > 1.
-      if ((!divArr[i].classList.contains('js-hide')) && (activeArr.length > 1)) {
-        let firstClassName = divArr[i].classList[0];
-        let mainNavItem = document.getElementsByClassName(firstClassName)[0];
-        // Un-mark respective main nav with a delay.
-        let removeActive = () => {
-          mainNavItem.classList.remove('js-nav-a-active');
-        };
-        let delayRemoveActive = setTimeout(removeActive, 600);
-      }
-    }
-  }, false);
-
-}());
-
-
-// On screen re-size, hide main nav, as long mobile nav is open.
-// Note: One additional line of code for this module under 'move mobile navigation container'.
 {
-  window.addEventListener('resize', () => {
-    let mobNavContainer = document.getElementsByClassName('js-mobNav-container')[0];
-        navUl = document.querySelector('nav ul');
+  let showDropdown = (e) => {
 
-    if (mobNavContainer.classList.contains('js-mobNav-slideIn')) {
-      navUl.style.display = 'none';
-    } else {
-      navUl.style.display = '';
+    if (e.target.classList.contains('js-nav-open')) {
+      clearTimeout(delayHide);
+
+      let show = () => {
+
+        let subMenu = document.getElementById('js-sub-menu');
+
+        // Hide all sub-menu content when hovering over menu-items.
+        let divArr = subMenu.querySelectorAll('div'); // Get all sub-menu divs.
+        for (let i = 0; i < divArr.length; i++) { // Add js-hide to all divs.
+          divArr[i].classList.add('js-hide');
+        }
+
+        // Show drop down container.
+        subMenu.classList.remove('js-hide');
+
+        // Show content-e.target
+        let triggerClass = e.target.classList[0]; // Get class that links to sub-menu content.
+        document.getElementsByClassName(triggerClass)[1].classList.remove('js-hide'); // Get 2nd elem w/that class, which is the sub-menu content, and display it.
+
+        // Shift drop down into place.
+
+        // Get a-tag position.
+        let rectTarget = e.target.getBoundingClientRect();
+        let leftEdgeOfTarget = rectTarget.left;
+        // Find drop down postion same as left edge of a-tag.
+        let headerOffsetLeft = document.getElementsByClassName('js-header')[0].offsetLeft;
+        let subMenuLeftPosition = leftEdgeOfTarget - headerOffsetLeft;
+        // Find length of drop down to center it under respective a-tag.
+        let subMenuWidth = subMenu.offsetWidth; // This has to run after we display both submenu and its child divs.
+        let aTagWidth = e.target.offsetWidth;
+        let adjustSubMenuPosition = (subMenuWidth - aTagWidth) / 2
+        // Set drop down position
+        subMenu.style.left = (subMenuLeftPosition - adjustSubMenuPosition) + 'px';
+      };
+
+      let delayShow = setTimeout(show, 200);
     }
-  });
-}
+  };
+
+  document.getElementsByTagName('nav')[0].addEventListener('mouseover', function (e) {
+    showDropdown(e);
+  }, false);
 
 
-// END MAIN NAVIGATION
+  // If mouseenter on drop down cancel timeout for closing it.
+  document.getElementById('js-sub-menu').addEventListener('mouseover', function () {
+    clearTimeout(delayHide);
+  }, false);
+
+  let delayHide;
+
+  let hideDropdown = (e) => {
+    if (!e.relatedTarget.classList.contains('js-nav-open')) {
+      let hide = () => {
+        let subMenu = document.getElementById('js-sub-menu');
+        // Hide drop down container.
+        subMenu.classList.add('js-hide');
+      };
+
+      delayHide = setTimeout(hide, 600);
+    }
+  };
+
+  // Loop over all class-names 'nav-open' and add eventlistener to them.
+  let navOpenArr = document.getElementsByClassName('js-nav-open');
+  for (let i = 0; i < navOpenArr.length; i++) {
+    navOpenArr[i].addEventListener('mouseleave', function (e) {
+      hideDropdown(e);
+    }, false);
+  };
+
+  // Dynamic active-menu indicator for main-nav.
+  (function () {
+    let currentPath = location.pathname,
+        aArr = document.querySelectorAll('nav a');
+
+    for (let i = 0; i < aArr.length; i++) {
+      // Get href of a tag.
+      let href = aArr[i].getAttribute('href');
+      // Get index position of href inside current path. If not -1 execute code.
+      if (currentPath.indexOf(href) !== -1) {
+        // Add active class to a elem.
+        aArr[i].classList.add('js-nav-a-active');
+      }
+    }
+  }());
+
+  // Dynamic active-menu indicator for sub-nav. And also for main nav.
+  (function () {
+    let currentPath = location.pathname,
+        subMenu = document.getElementsByClassName('nav-sub-menu')[0],
+        aArr = subMenu.getElementsByTagName('a');
+
+    for (let i = 0; i < aArr.length; i++) {
+      // Get href of a tag.
+      let href = aArr[i].getAttribute('href');
+      // Get index position of href inside current path. If not -1 execute code.
+      if (currentPath.indexOf(href) !== -1) {
+        // Add active class to a elem.
+        aArr[i].classList.add('js-nav-sub-menu-active');
+
+        // Apply active-menu indicator to parent main-nav items.
+        // Get a-tags parents first class name.
+        let firstClassNameOfParent = aArr[i].parentNode.classList[0];
+        // Apply active nav css to main nav item.
+        document.getElementsByClassName(firstClassNameOfParent)[0].classList.add('js-nav-a-active');
+      }
+    }
+  }());
+
+
+  // If hover/leave sub nav mark/un-markt respective main nav item.
+  (function() {
+    let subMenu = document.getElementById('js-sub-menu');
+    let divArr = subMenu.getElementsByTagName('div');
+
+    // If hovering over sub menu, mark respective nav item.
+    subMenu.addEventListener('mousemove', function () {
+      // Add event listener to all divs in sub menu.
+      for (let i = 0; i < divArr.length; i++) {
+        // Mark respective main nav item as active.
+        if (!divArr[i].classList.contains('js-hide')) {
+          let firstClassName = divArr[i].classList[0];
+          let mainNavItem = document.getElementsByClassName(firstClassName)[0];
+          mainNavItem.classList.add('js-nav-a-active');
+        }
+      }
+    }, false);
+
+    // If sub menu doesn't show, un-mark respective nav item.
+    subMenu.addEventListener('mouseleave', function (e) {
+      // Get number of active main-nav-items.
+      let activeArr = document.getElementsByClassName('js-nav-a-active');
+
+      for (let i = 0; i < divArr.length; i++) {
+        // Target respective sub nav div.
+        // If respective sub-nav-div contains class js-hide and
+        // if the number of active main-nav-items > 1.
+        if ((!divArr[i].classList.contains('js-hide')) && (activeArr.length > 1)) {
+          let firstClassName = divArr[i].classList[0];
+          let mainNavItem = document.getElementsByClassName(firstClassName)[0];
+          // Un-mark respective main nav with a delay.
+          let removeActive = () => {
+            mainNavItem.classList.remove('js-nav-a-active');
+          };
+          let delayRemoveActive = setTimeout(removeActive, 600);
+        }
+      }
+    }, false);
+  }());
+
+
+  // On screen re-size, hide main nav, as long mobile nav is open.
+  // Note: One additional line of code for this module under 'move mobile navigation container'.
+  {
+    window.addEventListener('resize', () => {
+      let mobNavContainer = document.getElementsByClassName('js-mobNav-container')[0];
+          navUl = document.querySelector('nav ul');
+
+      if (mobNavContainer.classList.contains('js-mobNav-slideIn')) {
+        navUl.style.display = 'none';
+      } else {
+        navUl.style.display = '';
+      }
+    });
+  }
+} // End main navigation.
 
 
 // START MOBILE NAVIGATION
+{
+  // Move slides inside navigation container.
 
-// MOVE SLIDES INSIDE NAVIGATION CONTAINER
+  let shiftSlide = (e) => {
 
-let shiftSlide = (e) => {
+    // Move clicked slide left/right depending on if prev/next button was clicked.
+    let shiftCurrentSlide = (shiftDir) => {
+      e.target.parentNode.classList.add(shiftDir);
+    };
 
-  // Move clicked slide left/right depending on if prev/next button was clicked.
-  let shiftCurrentSlide = (shiftDir) => {
-    e.target.parentNode.classList.add(shiftDir);
+    // Move linked slide left
+    let shiftLinkedSlideLeft = (className) => {
+      // Get all elems with class name.
+      let classCollection = document.getElementsByClassName(className);
+      // Loop over all elems and remove class that holds them right.
+      for (let i = 0; i < classCollection.length; i++) {
+        classCollection[i].classList.remove('js-mobNav-right');
+      }
+    };
+
+    // Move linked slides on left to center
+    let shiftLinkedSlideRight = () => {
+      let slidesOnLeft = document.getElementsByClassName('js-mobNav-left'),
+        topSlide = slidesOnLeft.length - 1;
+
+      slidesOnLeft[topSlide].classList.remove('js-mobNav-left');
+    };
+
+    // Logic
+    let shiftDir, className, idName;
+    // If link to new slide is clicked.
+    if (e.target.classList.contains('js-mobNav-next')) {
+      shiftCurrentSlide('js-mobNav-left');
+      idName = e.target.id; // Id on link
+      shiftLinkedSlideLeft(idName);
+
+      // If back link is clicked.
+    } else if (e.target.classList.contains('js-mobNav-prev')) {
+      shiftCurrentSlide('js-mobNav-right');
+      shiftLinkedSlideRight();
+    }
+
   };
 
-  // Move linked slide left
-  let shiftLinkedSlideLeft = (className) => {
-    // Get all elems with class name.
-    let classCollection = document.getElementsByClassName(className);
-    // Loop over all elems and remove class that holds them right.
-    for (let i = 0; i < classCollection.length; i++) {
-      classCollection[i].classList.remove('js-mobNav-right');
+  document.getElementsByClassName('js-mobNav-container')[0].addEventListener('click', function(e) {
+    shiftSlide(e);
+  }, false);
+
+
+  // Move mobile navigation container.
+
+  let shiftMobNav = (e) => {
+
+    let mobNavContainer = document.getElementsByClassName('js-mobNav-container')[0],
+      header = document.getElementsByClassName('js-header')[0],
+      pageLayover = document.getElementsByClassName('js-mobNav-pageLayover')[0];
+      navUl = document.querySelector('nav ul');
+
+    // If open buttons clicked, move mobile-nav-container in viewport and add page layover.
+    if (e.target.id === 'js-mobNav-open') {
+      mobNavContainer.classList.add('js-mobNav-slideIn');
+      pageLayover.classList.add('js-pageLayover-show');
+      // If close buttons clicked, or click outside header and outside mobile-nav-container move it out viewport.
+    } else if ((e.target.id === 'js-mobNav-close') || ((!header.contains(e.target)) && (!mobNavContainer.contains(e.target)))) {
+      mobNavContainer.classList.remove('js-mobNav-slideIn');
+      pageLayover.classList.remove('js-pageLayover-show');
+      navUl.style.display = ''; // Remove display-none from main-nav-ul. Needed for module: 'On screen re-size, hide main nav, as long mobile nav is open.'
     }
   };
 
-  // Move linked slides on left to center
-  let shiftLinkedSlideRight = () => {
-    let slidesOnLeft = document.getElementsByClassName('js-mobNav-left'),
-      topSlide = slidesOnLeft.length - 1;
+  document.body.addEventListener('click', function(e) {
+    shiftMobNav(e);
+  }, false);
 
-    slidesOnLeft[topSlide].classList.remove('js-mobNav-left');
-  };
-
-  // Logic
-  let shiftDir, className, idName;
-  // If link to new slide is clicked.
-  if (e.target.classList.contains('js-mobNav-next')) {
-    shiftCurrentSlide('js-mobNav-left');
-    idName = e.target.id; // Id on link
-    shiftLinkedSlideLeft(idName);
-
-    // If back link is clicked.
-  } else if (e.target.classList.contains('js-mobNav-prev')) {
-    shiftCurrentSlide('js-mobNav-right');
-    shiftLinkedSlideRight();
-  }
-
-};
-
-document.getElementsByClassName('js-mobNav-container')[0].addEventListener('click', function(e) {
-  shiftSlide(e);
-}, false);
+} // End mobile navigation.
 
 
-// MOVE MOBILE NAVIGATION CONTAINER
+// SCROLL TO TOP BUTTON.
 
-let shiftMobNav = (e) => {
-
-  let mobNavContainer = document.getElementsByClassName('js-mobNav-container')[0],
-    header = document.getElementsByClassName('js-header')[0],
-    pageLayover = document.getElementsByClassName('js-mobNav-pageLayover')[0];
-    navUl = document.querySelector('nav ul');
-
-  // If open buttons clicked, move mobile-nav-container in viewport and add page layover.
-  if (e.target.id === 'js-mobNav-open') {
-    mobNavContainer.classList.add('js-mobNav-slideIn');
-    pageLayover.classList.add('js-pageLayover-show');
-    // If close buttons clicked, or click outside header and outside mobile-nav-container move it out viewport.
-  } else if ((e.target.id === 'js-mobNav-close') || ((!header.contains(e.target)) && (!mobNavContainer.contains(e.target)))) {
-    mobNavContainer.classList.remove('js-mobNav-slideIn');
-    pageLayover.classList.remove('js-pageLayover-show');
-    navUl.style.display = ''; // Remove display-none from main-nav-ul. Needed for module: 'On screen re-size, hide main nav, as long mobile nav is open.'
-  }
-};
-
-document.body.addEventListener('click', function(e) {
-  shiftMobNav(e);
-}, false);
-
-// END MOBILE NAVIGATION
-
-
-// SCROLL TO TOP BUTTON
-
-{ // Start smooth scroll module.
+{
   let upButton = document.getElementById('js-button-to-top');
 
   // Display up-button if site is scrolled past 1300px (e.g. on re-fresh).
@@ -421,7 +417,7 @@ document.body.addEventListener('click', function(e) {
 
   upButton.addEventListener('click', smoothScroll, false);
 
-} // End smooth scroll module.
+} // End scroll to top button.
 
 
 // START FADE IN MAIN IMAGES IN INTRO SECTIONS.
@@ -440,10 +436,7 @@ document.body.addEventListener('click', function(e) {
 // DYNAMIC YEAR.
 
 {
-  // Get yearSpan.
-  let yearSpan = document.getElementById('year');
-  // Create txt node w dynamic year.
-  let currentYear = document.createTextNode(new Date().getFullYear());
-  // Append txt node to yearSpan.
+  let yearSpan = document.getElementById('year'),
+      currentYear = document.createTextNode(new Date().getFullYear());
   yearSpan.appendChild(currentYear);
 } // End dynamic year.
