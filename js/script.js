42953bafc4177e555fcb9058c938cad8c2ede84b
Christian Knauer <christiancalled@gmail.com>
2019-12-05 18:46:24 -0800

    In JS, make scroll-to-top quicker.

--- js/script.js
+++ js/script.js
@@ -450,7 +450,7 @@ $(document).ready(function() {
   let smoothScroll = () => {
     let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
     if (currentScroll > 0) {
-      window.scrollTo(0, currentScroll - (currentScroll / 13));
+      window.scrollTo(0, currentScroll - 25);
       window.requestAnimationFrame(smoothScroll);
     }
   };
